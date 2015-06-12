9801409396fa237bda91150db635271c7621ea62
elainapolson <elainapolson@gmail.com>
2015-06-12 15:26:15 -0400

    done

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
-  # code your solution here!
+
+  attr_accessor :name, :dog
+
+  def initialize(name, dog)
+    @name = name
+    @dog = dog
+  end
+
+  def prepare_items
+    self.dog.leash = true
+    self.dog.plastic_bag = true
+  end
+
+  def walk
+    prepare_items
+    self.dog.walking = true
+  end
+
+  def walking?
+    self.dog.walking
+  end
+
+  def vet_checkup
+    prepare_items
+    self.dog.vet_checkup = true
+  end
+  
 end
